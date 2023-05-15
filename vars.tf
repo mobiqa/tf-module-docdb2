61defbe7436222aaef8fb63ee0085929bcfc427a
venkat <venkat.mobiqa@gmail.com>
2023-05-15 07:37:51 +0530

    updating ./tf-module-docdb2

--- vars.tf
+++ vars.tf
@@ -3,6 +3,6 @@ variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
 variable "engine_version" {}
-#variable "number_of_instances" {}
-#variable "instance_class" {}
-#
+variable "number_of_instances" {}
+variable "instance_class" {}
+
